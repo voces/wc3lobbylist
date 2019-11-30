cbd3fcdfdbab12b72023d941b645a8ad3216c26a
Robert Coe <chakrawc@gmail.com>
2019-11-30 15:41:34 -0800

    tree tag

--- config.js
+++ config.js
@@ -1,15 +1,22 @@
 
 export default {
+	// sheep tag
 	"232301665666072577": {
 		filter: lobby => process.env.NODE_ENV === "production" &&
 			lobby.name.match( /^.*(sh(e{2,})p.*tag|\b(st)+\b|\bst[^a-z]|stbd|bdst).*$/i ) &&
 			! lobby.name.match( /soldier/i ) &&
 			! lobby.name.match( /civilization/i ),
 	},
+	// vamp zero
 	"650285615140569115": {
 		filter: lobby => process.env.NODE_ENV === "production" &&
 			lobby.name.match( /vamp.*zero/i ),
 		format: lobby => `<@&650378544479731712> [${lobby.server}] ${lobby.name} (${lobby.slots.occupied}/${lobby.slots.max})`,
+	},
+	// tree tag
+	"650062967903354901": {
+		filter: lobby => process.env.NODE_ENV === "production" &&
+			lobby.name.match( /tree.*tag/i ),
 	},
 	"457570641638326274": {
 		filter: lobby => process.env.NODE_ENV !== "production" &&
