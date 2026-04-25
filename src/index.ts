51e75968e9cde6153c683088c70662e60f6f1762
voces <chakrawc@gmail.com>
2026-04-25 01:18:53 -0700

    kill error reporting

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
 import "./commands/index.js";
 import "./close.js";
 import "./w3xio/index.js";
-
-import { messageAdmin } from "./discord.js";
-
-process.on("uncaughtException", async (err) => {
-	console.error(err);
-	try {
-		await messageAdmin(
-			`Uncaught exception:\n\`\`\`\n${err.stack ?? err}\n\`\`\``,
-		);
-	} finally {
-		process.exit(1);
-	}
-});
